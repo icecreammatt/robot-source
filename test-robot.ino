529d52eb99c827cfcca7626796d39dacecaf25e7
Matt Carrier <icecreammatt@gmail.com>
2020-05-22 03:19:50 -0700

    Working forward and backward movement with uno

--- test-robot.ino
+++ test-robot.ino
@@ -1,62 +1,67 @@
+/*
+  Pins for Mega
+  J1 2 PULL, 3 DIR
+  J2 4 PULL, 5 DIR
+  J3 6 PULL, 7 DIR
+  J4 8 PULL, 9 DIR
+  J5 10 PULL, 11 DIR
+  J6 12 PULL, 13 DIR
+  J7 20 PULL, 21 DIR
+*/
 
-
-int pulMinus = 9;  //PUL-
-int pulPlus = 10;  // PUL+5
-
-int dirMinus = 11;  // DIR-
-int dirPlus = 12;   // DIR+5
+// Using 8,9 for uno to test with
+int stepPin = 8;  //PUL-
+int directionPin = 9;  // DIR-
 
 int loops = 0;
+int pulseWidthMicros = 0;
+int millisbetweenSteps = 5; // milliseconds - or try 1000 for slower steps
+
+int maxStepsForward = 1000;
+int maxStepsBackward = 1000;
+int reset = maxStepsForward * 2;
 
 void setup() {
-  pinMode(pulMinus, OUTPUT);
-  pinMode(pulPlus, OUTPUT);
-  
-  pinMode(dirMinus, OUTPUT);
-  pinMode(dirPlus, OUTPUT);
-
-//  digitalWrite(dirMinus, HIGH); // for reverse 
-  digitalWrite(dirMinus, LOW); 
-  digitalWrite(dirPlus, LOW);
-
-//  digitalWrite(pulMinus, LOW); // for reverse
-  digitalWrite(pulMinus, HIGH);
-  digitalWrite(pulPlus, LOW);
-
-  
-  Serial.begin(9600);
   Serial.println("init");
+  Serial.begin(9600);
 
-  // Forward
-  digitalWrite(dirPlus, HIGH);
-  delay(100);
-  digitalWrite(dirPlus, LOW);
+  pinMode(directionPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
 
-  // Reverse
-//  digitalWrite(pulPlus, HIGH);
-//  delay(100);
-//  digitalWrite(pulPlus, LOW);
-  
+  digitalWrite(directionPin, HIGH);
+
+  delay(2000);
 }
 
 void forward() {
-    digitalWrite(pulPlus, HIGH);
-    digitalWrite(pulPlus, LOW);
+  // Serial.println("forward");
+  digitalWrite(directionPin, HIGH);
+  digitalWrite(stepPin, HIGH);
+  delay(pulseWidthMicros);
+  digitalWrite(stepPin, LOW);
+  delay(millisbetweenSteps);
 }
 
-void reverse() { 
-    digitalWrite(dirPlus, HIGH);
-    digitalWrite(dirPlus, LOW);
+void backward() {
+  // Serial.println("backward");
+  digitalWrite(directionPin, LOW); 
+  digitalWrite(stepPin, HIGH);
+  delay(pulseWidthMicros);
+  digitalWrite(stepPin, LOW);
+  delay(millisbetweenSteps);
 }
-
 
 void loop() {
-
-//   if(loops < 100) {
-    forward();
-    loops++;
-//   }
-
-  Serial.println(loops);
-  delay(1);
+    if (loops % 100 == 0) {
+      Serial.println(loops);
+    }
+    if (loops <= maxStepsForward) {
+      forward();
+      loops++;
+    } else if (loops >= maxStepsForward && loops <= reset){
+      backward();
+      loops++;
+    } else if (loops > reset) {
+      loops = 0;
+    }
 }
