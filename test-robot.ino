da24ecd3619f16f8212761f147c81167fe6c2556
Matt Carrier <icecreammatt@gmail.com>
2020-05-22 18:21:01 -0700

    Comments and minor tweaks

--- test-robot.ino
+++ test-robot.ino
@@ -3,40 +3,41 @@
   J1 2 PULL, 3 DIR - Rotate base
   J2 4 PULL, 5 DIR - Arm up and down
   J3 6 PULL, 7 DIR - Elbow up and down
-  J4 8 PULL, 9 DIR - Rotate Wrist
-  J5 10 PULL, 11 DIR - Roll Wrist // DANGER THIS HAS LIMITED RANGE (install limit switches before using)
-  J6 12 PULL, 13 DIR - Roll Hand
+  J4 8 PULL, 9 DIR - Rotate Elbow
+  J5 10 PULL, 11 DIR - Rotate Wrist // DANGER THIS HAS LIMITED RANGE (install limit switches before using)
+  J6 12 PULL, 13 DIR - Spin Hand
   J7 20 PULL, 21 DIR - Slide on rail, (not installed yet)
 */
+
+// Rotate base
 const int J1_STEP = 2;
 const int J1_DIR = 3;
 
+// Move Arm up and down
 const int J2_STEP = 4;
 const int J2_DIR = 5;
 
+// Move elbow up and down
 const int J3_STEP = 6;
 const int J3_DIR = 7;
 
+// Rotate elbow elbow
 const int J4_STEP = 8;
 const int J4_DIR = 9;
 
-// DANGER
-// const int J5_STEP = 10;
-// const int J5_DIR = 11;
+// DANGER  Rotate wrist
+const int J5_STEP = 10;
+const int J5_DIR = 11;
 
+// Spin hand
 const int J6_STEP = 12;
 const int J6_DIR = 13;
 
-// Using 8,9 for uno to test with
-// int stepPin = 2;  //PUL-
-// int directionPin = 3;  // DIR-
-
-int loops = 0;
+int steps = 0;
 int pulseWidthMicros = 1;
 int millisbetweenSteps = 0; // milliseconds - or try 1000 for slower steps
 
-int maxStepsForward = 2000;
-int maxStepsBackward = maxStepsForward;
+int maxStepsForward = 1000;
 int reset = maxStepsForward * 2;
 
 void setup() {
@@ -53,11 +54,10 @@ void setup() {
     }
   }
 
-  delay(2000);
+  delay(5000);
 }
 
 void forward(int stepPin, int directionPin) {
-  // Serial.println("forward");
   digitalWrite(directionPin, HIGH);
   digitalWrite(stepPin, HIGH);
   delay(pulseWidthMicros);
@@ -66,7 +66,6 @@ void forward(int stepPin, int directionPin) {
 }
 
 void backward(int stepPin, int directionPin) {
-  // Serial.println("backward");
   digitalWrite(directionPin, LOW); 
   digitalWrite(stepPin, HIGH);
   delay(pulseWidthMicros);
@@ -75,24 +74,33 @@ void backward(int stepPin, int directionPin) {
 }
 
 void loop() {
-    if (loops % 100 == 0) {
-      Serial.println(loops);
+
+    if (steps % 100 == 0) {
+      Serial.println(steps);
     }
-    if (loops <= maxStepsForward) {
+
+    if (steps <= maxStepsForward) {
       forward(J1_STEP, J1_DIR);
       forward(J2_STEP, J2_DIR);
       backward(J3_STEP, J3_DIR);
-      // forward(J4_STEP, J4_DIR);
       backward(J6_STEP, J6_DIR);
-      loops++;
-    } else if (loops >= maxStepsForward && loops <= reset){
+      forward(J4_STEP, J4_DIR); // twist axis
+
+      // backward(J5_STEP, J5_DIR); // DANGER Enable with Caution
+
+      steps++;
+    } else if (steps >= maxStepsForward && steps <= reset) {
       backward(J1_STEP, J1_DIR);
       backward(J2_STEP, J2_DIR);
       forward(J3_STEP, J3_DIR);
-      // backward(J4_STEP, J4_DIR);
       forward(J6_STEP, J6_DIR);
-      loops++;
-    } else if (loops > reset) {
-      loops = 0;
+      backward(J4_STEP, J4_DIR); // twist axis
+
+      // forward(J5_STEP, J5_DIR); // DANGER Enable with Caution
+
+      steps++;
+    } else if (steps > reset) {
+      steps = 0;
     }
+
 }
