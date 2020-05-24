6167e45e6f6361093f3711719916ae6e0c1d2c3f
Matt Carrier <icecreammatt@gmail.com>
2020-05-23 18:18:31 -0700

    Add support for limit switches to disable motors

--- test-robot.ino
+++ test-robot.ino
@@ -9,6 +9,16 @@
   J7 20 PULL, 21 DIR - Slide on rail, (not installed yet)
 */
 
+boolean enableMovement = false;
+
+boolean J1_enabled = false;
+boolean J2_enabled = false;
+boolean J3_enabled = false;
+boolean J4_enabled = false;
+boolean J5_enabled = false;
+boolean J6_enabled = false;
+boolean J7_enabled = false;
+
 // Rotate base
 const int J1_STEP = 2;
 const int J1_DIR = 3;
@@ -44,6 +54,7 @@ void setup() {
   Serial.println("init");
   Serial.begin(9600);
 
+ // Setup output / movement pins
   Serial.println("setup pin modes");
   for (int i = 2; i <= 13; i++) {
     pinMode(i, OUTPUT);
@@ -54,7 +65,42 @@ void setup() {
     }
   }
 
-  delay(5000);
+  // Setup input relays
+  Serial.println("setup relay pin modes");
+  for (int i = 14; i <= 19; i++) {
+    pinMode(i, INPUT);
+  }
+
+  Serial.println("Power up drivers... movement starting program in 5 seconds");
+  delay(1000);
+  Serial.println("4");
+  delay(1000);
+  Serial.println("3");
+  delay(1000);
+  Serial.println("2");
+  delay(1000);
+  Serial.println("1");
+  delay(1000);
+
+/* Test J5 movement
+  enableMovement = true;
+  J1_enabled = false;
+  J2_enabled = false;
+  J3_enabled = false;
+  J4_enabled = false;
+  J5_enabled = true; // DANGER
+  J6_enabled = false;
+*/
+
+  enableMovement = true;
+  J1_enabled = true;
+  J2_enabled = true;
+  J3_enabled = true;
+  J4_enabled = true;
+  J5_enabled = false; // DANGER
+  J6_enabled = true;
+
+  Serial.println("Moving...");
 }
 
 void forward(int stepPin, int directionPin) {
@@ -73,30 +119,58 @@ void backward(int stepPin, int directionPin) {
   delay(millisbetweenSteps);
 }
 
+void readRelayState() {
+  for (int i = 14; i <= 19; i++) {
+    int readValue = digitalRead(i);
+    if (i == 14 && readValue == 1) {
+      J1_enabled = false;
+      J5_enabled = false;
+    } else if (i == 15 && readValue == 1) {
+      J2_enabled = false;
+    } else if (i == 16 && readValue == 1) {
+      J3_enabled = false;
+    } else if (i == 17 && readValue == 1) {
+      J4_enabled = false;
+    } else if (i == 18 && readValue == 1) {
+      J5_enabled = false;
+    } else if (i == 19 && readValue == 1) {
+      J6_enabled = false;
+    }
+  }
+}
+
 void loop() {
 
-    if (steps % 100 == 0) {
+    if (steps % 2000 == 0) {
       Serial.println(steps);
     }
 
-    if (steps <= maxStepsForward) {
-      forward(J1_STEP, J1_DIR);
-      forward(J2_STEP, J2_DIR);
-      backward(J3_STEP, J3_DIR);
-      backward(J6_STEP, J6_DIR);
-      forward(J4_STEP, J4_DIR); // twist axis
+    readRelayState();
 
-      // backward(J5_STEP, J5_DIR); // DANGER Enable with Caution
+    if (steps <= maxStepsForward) {
+
+      if (enableMovement) {
+        if (J1_enabled) forward(J1_STEP, J1_DIR);
+        if (J2_enabled) forward(J2_STEP, J2_DIR);
+        if (J3_enabled) backward(J3_STEP, J3_DIR);
+        if (J6_enabled) backward(J6_STEP, J6_DIR);
+        if (J4_enabled) forward(J4_STEP, J4_DIR); // twist axis
+
+        // if (J5_enabled) backward(J5_STEP, J5_DIR); // DANGER Enable with Caution
+      }
 
       steps++;
     } else if (steps >= maxStepsForward && steps <= reset) {
-      backward(J1_STEP, J1_DIR);
-      backward(J2_STEP, J2_DIR);
-      forward(J3_STEP, J3_DIR);
-      forward(J6_STEP, J6_DIR);
-      backward(J4_STEP, J4_DIR); // twist axis
 
-      // forward(J5_STEP, J5_DIR); // DANGER Enable with Caution
+      if (enableMovement) {
+        if (J1_enabled) backward(J1_STEP, J1_DIR);
+        if (J2_enabled) backward(J2_STEP, J2_DIR);
+        if (J3_enabled) forward(J3_STEP, J3_DIR);
+        if (J6_enabled) forward(J6_STEP, J6_DIR);
+        if (J4_enabled) backward(J4_STEP, J4_DIR); // twist axis
+
+        // if (J5_enabled) forward(J5_STEP, J5_DIR); // DANGER Enable with Caution
+      }
 
       steps++;
     } else if (steps > reset) {
